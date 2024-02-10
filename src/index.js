f2082ada2a7830ce22b98052339eb0326273c521
wwartick <willwartick@gmail.com>
2024-02-10 15:57:53 -0800

    asset management done

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import _ from 'lodash';
 function component() {
     const element = document.createElement('div');
   
-    // Lodash, now imported by this script
+   
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+   
+
     return element;
   }
   
